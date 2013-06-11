30713d0371d43baff1a17a94d20971293d5edbbe
Matheus Tardivo <matheustardivo@gmail.com>
2013-06-11 16:31:42 -0300

    Refactor to App.java

--- src/main/java/net/tardivo/spring/redis/cache/App.java
+++ src/main/java/net/tardivo/spring/redis/cache/App.java
@@ -2,26 +2,24 @@ package net.tardivo.spring.redis.cache;
 
 import lombok.extern.slf4j.Slf4j;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 
 @Slf4j
 public class App {
 
 	public static void main(String[] args) {
-		ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfiguration.class);
+		AbstractApplicationContext ctx = new AnnotationConfigApplicationContext(SpringConfiguration.class);
+		ctx.registerShutdownHook();
 
 		HelloService helloService = ctx.getBean(HelloService.class);
 
-		log.info(helloService.getMessage("Matheus"));
-		log.info(helloService.getMessage("Matheus"));
-		log.info(helloService.getMessage("Matheus"));
-		log.info(helloService.getMessage("Matheus"));
+		for (int i = 0; i < 1000; i++) {
+			log.info(helloService.getMessage("Matheus"));
 
-		log.info("{}", helloService.getHello("Matheus", "Opa"));
-		log.info("{}", helloService.getHello("Matheus", "Opa"));
+			log.info("{}", helloService.getHello("Matheus", "Opa"));
 
-		log.info("{}", helloService.getAllHello(new Hello("Matheus", "Opa")));
-		log.info("{}", helloService.getAllHello(new Hello("Matheus", "Opa")));
+			log.info("{}", helloService.getAllHello(new Hello("Matheus", "Opa")));
+		}
 	}
 }
